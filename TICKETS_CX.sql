101dbad4cc1c572b612c2fa0283289c72a2a428e
MUJALAL <108790857+MUJALAL@users.noreply.github.com>
2022-07-19 18:18:17 +0530

    Update TICKETS_CX.sql

--- TICKETS_CX.sql
+++ TICKETS_CX.sql
@@ -1,4 +1,4 @@
-1. At Issue Level 
+-- 1. At Issue Level 
 
 SELECT 
 	 Reason,
@@ -24,3 +24,31 @@ ON
 	sfc.id = sfcd.case_cc_id
 ) as t1
 GROUP BY 1
+
+
+
+-- 2. At Partner and Customer Level 
+
+SELECT 
+	 raised_by,
+	 Reason,
+	 count(DISTINCT csid) FILTER(WHERE status in ('Open', 'Closed')) as Total_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE status = 'Open') as Total_Open_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE status = 'Closed') as Total_Closed_Tickets
+FROM
+(
+SELECT
+	sfc.id as csid,
+	raised_by,
+	sfc.status,
+	case_type,
+	unnest(string_to_array(issue_v2,';')) as Reason
+FROM 
+	sf_cases as sfc
+JOIN
+	sf_case_cc_details as sfcd
+ON 
+	sfc.id = sfcd.case_cc_id
+) as t1
+WHERE raised_by in ('Customer', 'Partner')
+GROUP BY 1,2
