aec6c4727474669d44a4c8ed98f14359199fba35
MUJALAL <108790857+MUJALAL@users.noreply.github.com>
2022-07-19 19:20:14 +0530

    Update TICKETS_CX.sql

--- TICKETS_CX.sql
+++ TICKETS_CX.sql
@@ -52,3 +52,36 @@ ON
 ) as t1
 WHERE raised_by in ('Customer', 'Partner')
 GROUP BY 1,2
+
+-- 3. At Geo Region Level
+
+--  finding partner tickets and which issue they belonged too, same with customers
+
+SELECT 
+	 genam,
+	 count(DISTINCT csid) FILTER(WHERE status in ('Open', 'Closed')) as Total_open_close_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE status = 'Open') as Total_Open_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE status = 'Closed') as Total_Closed_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE raised_by in ('Customer', 'Partner')) as Total_Tickets,
+	 count(DISTINCT csid) FILTER(WHERE raised_by  = 'Customer') as Total_Customer,
+	 count(DISTINCT csid) FILTER(WHERE raised_by  = 'Partner') as Total_Partner
+	 
+FROM
+(
+SELECT
+	sfc.id as csid,
+	geo_region_name as genam,
+	raised_by,
+	sfc.status,
+	case_type,
+	unnest(string_to_array(issue_v2,';')) as Reason
+FROM 
+	sf_cases as sfc
+JOIN
+	sf_case_cc_details as sfcd
+ON 
+	sfc.id = sfcd.case_cc_id
+) as t1
+WHERE raised_by in ('Customer', 'Partner')
+GROUP BY 1
+ 
