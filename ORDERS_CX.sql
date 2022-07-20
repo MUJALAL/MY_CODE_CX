4ef062302ae8371330105dfdadde96a03f453b51
MUJALAL <108790857+MUJALAL@users.noreply.github.com>
2022-07-20 15:23:17 +0530

    Update ORDERS_CX.sql

--- ORDERS_CX.sql
+++ ORDERS_CX.sql
@@ -1,4 +1,4 @@
-1. PREVIOUS FIVE DAYS DATA
+--1. LAST FIVE DAYS DATA
 
 SELECT
     Day,
@@ -81,3 +81,37 @@ ORDER BY 1
 
 
 
+--2. Fetching Data at Vehicle Level
+
+-- 1. finding customer (caller_type 2)calls in the last week, 
+
+SELECT Vname,
+	   COUNT(DISTINCT oid) as Total_count,
+	   COUNT(DISTINCT oid) FILTER(WHERE ostat = 4) as Completed_Order,
+	   COUNT(DISTINCT oid) FILTER(WHERE ostat = 5) as Cancelled_Order
+
+FROM
+(
+SELECT 
+	veh.display_name as Vname,
+	ord.order_id as oid,
+	ord.status as ostat
+	
+FROM 
+	orders as ord
+JOIN
+	vehicles as veh
+ON 
+	ord.vehicle_id = veh.id
+WHERE 
+	ord.status in (4,5)
+	AND
+	ord.order_type = 0
+	AND
+	ord.deleted_at is NULL
+	AND	
+	ord.pickup_time >= EXTRACT(EPOCH FROM CURRENT_DATE::TIMESTAMP - INTERVAL '5 Days') - 19800
+	AND
+	ord.pickup_time < EXTRACT(EPOCH FROM CURRENT_DATE::TIMESTAMP) - 19800 
+) as t1
+GROUP BY 1
