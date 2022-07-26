c56217bba7a8a97e1dcf96d8c468e71865ded82c
MUJALAL <108790857+MUJALAL@users.noreply.github.com>
2022-07-26 14:51:18 +0530

    Create Total_Number_of_calls.sql

---
+++ CALLER_RESPONSE/Total_Number_of_calls.sql
@@ -0,0 +1,20 @@
+SELECT Day,
+		count(DISTINCT customer_crt_id) FILTER (WHERE caller_type in (1,2)) as Total_Calls,
+		count(DISTINCT customer_crt_id) FILTER (WHERE caller_type = 1) as Partner_Calls,
+		count(DISTINCT customer_crt_id) FILTER (WHERE caller_type = 2) as Customer_Calls
+FROM
+(
+SELECT 
+	DATE(response_ts + INTERVAL '5 hours 30 minutes') as Day,
+	customer_crt_id,
+	customer_id,
+	driver_id,
+	caller_type
+FROM 
+	caller_responses
+WHERE 
+	response_ts >= CURRENT_DATE::TIMESTAMP - INTERVAL '5 Days 5 hours 30 minutes'
+	AND
+	response_ts < CURRENT_DATE::TIMESTAMP - INTERVAL '5 hours 30 minutes'
+) as calls
+GROUP BY 1
