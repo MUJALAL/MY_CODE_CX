b6877f6fbdfa9d893f77da4b1f3c5b2135054f49
MUJALAL <108790857+MUJALAL@users.noreply.github.com>
2022-08-11 11:48:09 +0530

    Update ticket_escalation.sql

--- TICKETS_CX/ticket_escalation.sql
+++ TICKETS_CX/ticket_escalation.sql
@@ -47,3 +47,47 @@ issue_v2, COUNT(DISTINCT id) FILTER(WHERE actions_taken_v2 ILIKE '%Escalate To%'
 FROM raw_data 
 GROUP BY 1
 ) as raw_data2;
+
+
+
+--------3.
+
+with raw_data AS (
+SELECT 
+	id, order_stage_v2, issue_v2, actions_taken_v2, extra_data -> 'Action_Comments__c' as ticket_comment
+
+FROM sf_case_cc_details
+
+WHERE actions_taken_v2 ILIKE '%Escalate To%'
+	AND		
+	created_at BETWEEN
+	CURRENT_DATE::TIMESTAMP - INTERVAL '5 Days 5 hours 30 minutes'
+	AND
+ 	CURRENT_DATE::TIMESTAMP - INTERVAL '5 hours 30 minutes' 
+)
+
+
+	
+, raw_data_2 as (SELECT 
+issue_v2, count(DISTINCT id) as Escalate_count
+FROM raw_data 
+GROUP BY 1
+ORDER BY 2 DESC
+)
+
+SELECT issue_v2, ticket_comment
+FROM raw_data
+WHERE issue_v2 in (SELECT issue_v2 FROM raw_data_2 where Escalate_count > 500)
+ORDER BY 1 ASC;
+
+
+
+SELECT ((Escalted_Total)*1.00/Total)
+FROM (
+SELECT COUNT(DISTINCT id) as Total , COUNT(DISTINCT id) FILTER(WHERE actions_taken_v2 ILIKE '%Escalate To%') as Escalted_Total
+FROM sf_case_cc_details
+WHERE created_at BETWEEN
+	CURRENT_DATE::TIMESTAMP - INTERVAL '5 Days 5 hours 30 minutes'
+	AND
+ 	CURRENT_DATE::TIMESTAMP - INTERVAL '5 hours 30 minutes' 
+) as rd ;
